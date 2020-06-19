c43b5f41b3d4cfac32a1dc910ebc235fe2bfb80d
Jasper Krauter <jasper.krauter@uni-konstanz.de>
2020-06-19 09:52:21 +0200

    overhaul of prettyPrint, so we're not bound to width or height anymore

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -1,24 +1,15 @@
 type Loc = (Integer, Integer)
 type Map = [Loc]
 
-width :: Integer
-width = 5
-
-height :: Integer
-height = 5
-
 example :: Map
-example = [(1,2), (2,2), (3,2)]
+example = [(0,0), (0,1), (0,2), (1,0), (1,2), (2,0), (2,1), (2,2), (3,0), (3,1), (3,2), (4,0), (4,1), (4,2), (5,0), (5,1), (5,2), (6,0), (6,2), (7,0), (7,1), (7,2)]
 
 removeDups :: Eq a => [a] -> [a]
 removeDups [] = []
 removeDups (x:xs) = x : (removeDups $ filter (/= x) xs)
 
-wrap :: Loc -> Loc
-wrap (x,y) = (mod x width, mod y height)
-
 neighbors :: Loc -> [Loc]
-neighbors (x,y) = map wrap [(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y), (x+1, y), (x-1, y+1), (x, y+1), (x+1, y+1)]
+neighbors (x,y) = [(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y), (x+1, y), (x-1, y+1), (x, y+1), (x+1, y+1)]
 
 countNeighbors :: Loc -> Map -> Int
 countNeighbors l m = length $ filter (flip elem m) $ neighbors l
@@ -32,17 +23,20 @@ births m = [l | l <- removeDups (concat (map neighbors m)), not $ elem l m, coun
 nextgen :: Map -> Map
 nextgen m = survivors m ++ births m
 
-pp :: Map -> IO [[()]]
-pp m = sequence [ppl x | x <- [0..height-1]]
+prettyPrint :: Map -> IO [()]
+prettyPrint m = prettyPrint' m (minimum xs - 1) (maximum xs + 1) (minimum ys - 1) (maximum ys + 1)
   where
-    ppl x = sequence [putStr $ getStr x y | y <- [0..width]]
-    getStr x y | y == width = "\n"
-               | elem (x,y) m = "#"
-               | otherwise = "."
+    xs = map fst m
+    ys = map snd m
+    prettyPrint' m minX maxX minY maxY = sequence [printLn x | x <- [minX..maxX]]
+      where
+        printLn x = putStrLn [getCh x y | y <- [minY..maxY]]
+        getCh x y | elem (x,y) m = '#'
+                  | otherwise = '.'
 
 life :: Map -> IO ()
 life m = do
-          pp m
+          prettyPrint m
           cmd <- getLine
           if cmd == "q" then return () else life (nextgen m)
 
